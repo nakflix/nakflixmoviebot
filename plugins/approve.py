368b03e9f7a77aa29220780c566d97bf4306b3c1
nakflix <123237114+nakflix@users.noreply.github.com>
2025-01-30 08:25:44 +0300

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -7,14 +7,14 @@ from pyrogram.errors import UserIsBlocked, PeerIdInvalid
 async def accept_request(client, r):
 
     rm = InlineKeyboardMarkup([[
-        InlineKeyboardButton("❤️‍🔥 Netflix Extra 🎥 ❤️‍🔥", url="https://t.me/netflixextra5"),
-        InlineKeyboardButton("⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡", url="https://t.me/netflixextra5")
+        InlineKeyboardButton("❤️‍🔥 𝗡𝗔𝗞𝗙𝗟𝗜𝗫𝗧𝗩 🎥 ❤️‍🔥", url="https://t.me/NAKFLIXTV"),
+        InlineKeyboardButton("⚡𝗡𝗔𝗞𝗙𝗟𝗜𝗫𝗣𝗟𝗨𝗦 ⚡", url="https://t.me/NAKFLIXPLUS")
     ]])
     
     try:
         await client.send_photo(
             r.from_user.id,
-            'https://graph.org/file/7b4da5aaa4ce6dc1806d2.jpg',
+            'https://telegra.ph/file/25a9d291a2b64dec90426.jpg',
             f"**𝖧𝖾𝗅𝗅𝗈 {r.from_user.mention} 👻, 𝖶𝖾𝗅𝖼𝗈𝗆𝖾 𝖳𝗈 {r.chat.title}\n𝖸𝗈𝗎𝗋 𝖱𝖾𝗊𝗎𝖾𝗌𝗍 𝖧𝖺𝗌 𝖡𝖾𝖾𝗇 𝖠𝗉𝗉𝗋𝗈𝗏𝖾𝖽...!!!**",
             reply_markup=rm)
 
