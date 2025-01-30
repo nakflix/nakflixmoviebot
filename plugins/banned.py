8f645490ec0e7464b9e61ec80c29496d80bdeb42
nakflix <123237114+nakflix@users.noreply.github.com>
2025-01-30 08:27:16 +0300

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -26,7 +26,7 @@ async def ban_reply(bot, message):
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('🧩 𝖲𝖴𝖯𝖯𝖮𝖱𝖳 🧩', url="https://t.me/+N11vNgcMFlwxZjc8")
+        InlineKeyboardButton('🧩 𝖲𝖴𝖯𝖯𝖮𝖱𝖳 🧩', url="https://t.me/NAKFLIXTV")
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
