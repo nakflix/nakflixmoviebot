297bf97a61fee069ad157d492d8e068999ca25e6
nakflix <123237114+nakflix@users.noreply.github.com>
2025-05-27 16:02:47 +0300

    Update info.py

--- info.py
+++ info.py
@@ -37,10 +37,10 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/c9a73d4c3831eff5fd
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1458235021').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002368928014').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001899642588').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL', '-1002368928014')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001899642588')
 auth_grp = environ.get('-1001654755951')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
