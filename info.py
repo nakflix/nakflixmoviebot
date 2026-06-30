0062f51a7325719b03af832e8dc668887adb1f7e
nakflix <123237114+nakflix@users.noreply.github.com>
2026-06-30 14:19:30 +0300

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ SUPPORT_CHAT_ID = -1001654755951
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nakflixbot:alpha3720@nakflixbot.gbt7w.mongodb.net/?retryWrites=true&w=majority&appName=nakflixbot")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nakflixbot:alpha3720@nakflixbot.gbt7w.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "nakflixbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'TechVJBot')
 
